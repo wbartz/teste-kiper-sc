19802b2ccf617bfc2eb1a17846143bd2fb05d85c
William Bartz <williamf.brtz@gmail.com>
2020-04-15 21:17:54 -0300

    Correção seed de usuários; Correção login;

--- api-nexthouse/src/database/seeders/20200415204631-Users.js
+++ api-nexthouse/src/database/seeders/20200415204631-Users.js
@@ -6,7 +6,9 @@ module.exports = {
      {
        full_name: 'Admin NextHouse',
        email: 'admin@nexthouse.com',
-       password: '5732c10582791878a1ddf059230392d400886d4837178054db1c6d49d4e03311'
+       password: '5732c10582791878a1ddf059230392d400886d4837178054db1c6d49d4e03311',
+       createdAt: new Date(),
+       updatedAt: new Date(),
      }
    ])
   },
